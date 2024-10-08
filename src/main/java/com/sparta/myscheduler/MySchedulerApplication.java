e7015474cd94cd9fa25a29228f41d6bd3dfa2c5c
wootaepark <pakdal99@gmail.com>
2024-10-08 12:08:26 +0900

    initial commit

---
+++ src/main/java/com/sparta/myscheduler/MySchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/myscheduler/MySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
